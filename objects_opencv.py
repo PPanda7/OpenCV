6a5c7dafc2e6a2a650c122d73badb92f532a1797
PPanda7 <136167801+PPanda7@users.noreply.github.com>
2024-05-20 11:47:10 +0300

    Update objects_opencv.py

--- objects_opencv.py
+++ objects_opencv.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # Загрузка изображения
-image = cv2.imread('woman_man.avif')
+image = cv2.imread('man.jpg')
 
 # Преобразование изображения в черно-белое
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
